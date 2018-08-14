f2ea0ba3a5c1fe7bede13dc78bff6de33d6fdf50
Laura R. Ojeda <rojedalaura@gmail.com>
2018-08-14 15:40:50 +0200

    course detail

--- routes/courses.js
+++ routes/courses.js
@@ -6,7 +6,19 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   Course.find({})
     .then((coursesArray) => {
-      res.render('courses', { coursesArray });
+      res.render('courses/list', { coursesArray });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Course.findById(id)
+    .then((course) => {
+      console.log(course)
+      res.render('courses/detail', course);
     })
     .catch((error) => {
       next(error);
