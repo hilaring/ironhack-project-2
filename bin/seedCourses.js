cd2f870219b9eb93aaadf3228c6559bbcdb09cc4
Laura R. Ojeda <rojedalaura@gmail.com>
2018-08-23 17:21:46 +0200

    new seed and fix course model

--- bin/seedCourses.js
+++ bin/seedCourses.js
@@ -10,7 +10,7 @@ const courses = [
     resume: 'Cómo instalar, configurar, entender los informes y tomar decisiones que nos ayuden a mejorar nuestro SEO, usabilidad y objetivos',
     temary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aspernatur fugiat saepe reprehenderit non amet esse, modi autem possimus nihil, accusantium ad? Adipisci accusantium corrupti cum qui laboriosam pariatur natus?',
     category: 'Marketing digital',
-    video: 'https://www.youtube.com/watch?v=P3V01bDbIR0',
+    video: 'https://www.youtube.com/embed/P3V01bDbIR0',
   },
   {
     name: 'Google AdWords',
@@ -18,7 +18,7 @@ const courses = [
     resume: '¿Sabéis que Google AdWords es la red de publicidad más grande del mundo? Y no es de extrañar, porque Google es la web más visitada del mundo, y por esa razón todo el mundo quiere anunciarse ahí.',
     temary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aspernatur fugiat saepe reprehenderit non amet esse, modi autem possimus nihil, accusantium ad? Adipisci accusantium corrupti cum qui laboriosam pariatur natus?',
     category: 'Marketing digital',
-    video: 'https://www.youtube.com/watch?v=P3V01bDbIR0',
+    video: 'https://www.youtube.com/embed/P3V01bDbIR0',
   },
   {
     name: 'WordPress básico',
@@ -26,7 +26,7 @@ const courses = [
     resume: 'WordPress es sin duda alguna, el mejor gestor de contenidos del mundo, y el más utilizado con diferencia, llegando actualmente al superar el 58% de cuota de mercado. Y encima es open source y completamente gratuito.',
     temary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aspernatur fugiat saepe reprehenderit non amet esse, modi autem possimus nihil, accusantium ad? Adipisci accusantium corrupti cum qui laboriosam pariatur natus?',
     category: 'WordPress',
-    video: 'https://www.youtube.com/watch?v=P3V01bDbIR0',
+    video: 'https://www.youtube.com/embed/P3V01bDbIR0',
   },
   {
     name: 'SEO para WordPress',
@@ -34,7 +34,7 @@ const courses = [
     resume: 'WordPress es un CMS fantástico, y viene muy preparado para posicionar bien. Sin embargo, hay varias cosas que se deja en el tintero que pueden ayudar a posicionar mejor una web.',
     temary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio aspernatur fugiat saepe reprehenderit non amet esse, modi autem possimus nihil, accusantium ad? Adipisci accusantium corrupti cum qui laboriosam pariatur natus?',
     category: ['WordPress', 'SEO'],
-    video: 'https://www.youtube.com/watch?v=P3V01bDbIR0',
+    video: 'https://www.youtube.com/embed/3V01bDbIR0',
   },
 ];
 
