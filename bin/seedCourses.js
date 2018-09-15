49fea9df084c65c2ffa7160cd070475ec3711e02
Laura R. Ojeda <rojedalaura@gmail.com>
2018-09-15 13:02:17 +0200

    fix error in sort courses and flash notifications

--- bin/seedCourses.js
+++ bin/seedCourses.js
@@ -8,7 +8,7 @@ Course.collection.drop();
 const courses = [
   {
     name: 'Google Analytics',
-    teacher: undefined,
+    teacher: '5b9ce357676ac112ec523c8d',
     image: 'https://boluda.com/files/curso-google-analytics-300x157.png',
     category: 'Marketing digital',
     resume: 'Cómo instalar, configurar, entender los informes y tomar decisiones que nos ayuden a mejorar nuestro SEO, usabilidad y objetivos',
@@ -19,7 +19,7 @@ const courses = [
   },
   {
     name: 'Google AdWords',
-    teacher: undefined,
+    teacher: '5b9ce357676ac112ec523c8d',
     image: 'https://boluda.com/files/curso-online-adwords-300x157.png',
     category: 'Marketing digital',
     resume: '¿Sabéis que Google AdWords es la red de publicidad más grande del mundo? Y no es de extrañar, porque Google es la web más visitada del mundo, y por esa razón todo el mundo quiere anunciarse ahí.',
@@ -30,7 +30,7 @@ const courses = [
   },
   {
     name: 'WordPress básico',
-    teacher: undefined,
+    teacher: '5b9ce357676ac112ec523c8d',
     image: 'https://boluda.com/files/curso-wordpress-300x157.png',
     category: 'WordPress',
     resume: 'WordPress es sin duda alguna, el mejor gestor de contenidos del mundo, y el más utilizado con diferencia, llegando actualmente al superar el 58% de cuota de mercado. Y encima es open source y completamente gratuito.',
@@ -41,7 +41,7 @@ const courses = [
   },
   {
     name: 'SEO para WordPress',
-    teacher: undefined,
+    teacher: '5b9ce357676ac112ec523c8d',
     image: 'https://boluda.com/files/curso-wordpress-300x157.png',
     category: ['WordPress', 'SEO'],
     resume: 'WordPress es un CMS fantástico, y viene muy preparado para posicionar bien. Sin embargo, hay varias cosas que se deja en el tintero que pueden ayudar a posicionar mejor una web.',
